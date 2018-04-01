a21a37760f05a0121df46e11880d67cd64a5da59
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-04-01 11:53:21 +0800

    Membuat Atribut dan Behavior

--- src/praktek02/handphone.java
+++ src/praktek02/handphone.java
@@ -1,4 +1,13 @@
 package praktek02;
+
 public class handphone {
-    
-}
+    int harga_handphone;
+    int jumlah_handphone;
+
+    void CetakInfo () {
+        System.out.println("=============================");
+        System.out.println("harga_handphone      :"+harga_handphone);
+        System.out.println("jumlah_handpone     :"+jumlah_handphone);
+        System.out.println("=============================");
+        }
+    }

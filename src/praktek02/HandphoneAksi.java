0635cab7a66ef059b83000eff096916514500929
fitryannugrahaa <32404079+fitryannugrahaa@users.noreply.github.com>
2018-04-01 12:07:32 +0800

    Mengisi Nilai Atribut

--- src/praktek02/HandphoneAksi.java
+++ src/praktek02/HandphoneAksi.java
@@ -3,5 +3,8 @@ package praktek02;
 public class HandphoneAksi {
     public static void main(String[] args) {
         handphone r1 = new handphone();
+
+        r1.harga_handphone = 4000000;
+        r1.jumlah_handphone = 2;
     }
 }
